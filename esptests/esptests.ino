6fe093f8c7be9b2e15192e45369d3f8b20b181af
Trevor Merrifield <trevorm42@gmail.com>
2018-02-14 22:06:25 -0500

    Working sawtooth

--- esptests/esptests.ino
+++ esptests/esptests.ino
@@ -9,16 +9,17 @@ unsigned last;
 
 void setup() {
     pinMode(D8, OUTPUT);
-    analogWriteFreq(440);
+    analogWriteFreq(1000);
     analogWrite(D8, 512);
     last = micros();
 }
 
 void loop() {
     unsigned now = micros();
-    if (now > last + 2273/1024) {
+    if (now > last + 22730/1024) {
         anything = (anything + 1) % 1024;
         last = now;
         analogWrite(D8, anything);
     }
+
 }
